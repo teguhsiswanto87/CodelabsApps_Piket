240492b6707978a3f3e483136a7e03ae72333c93
Teguh Siswanto <teguhsiswanto@email.unikom.ac.id>
2019-10-19 23:12:10 +0700

    initial commit

---
+++ app/src/main/java/id/codelabs/codelabsapps_piket/ui/MainActivity.kt
@@ -0,0 +1,13 @@
+package id.codelabs.codelabsapps_piket.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import id.codelabs.codelabsapps_piket.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/id/codelabs/codelabsapps_piket/ui/login/LoginActivity.kt
@@ -0,0 +1,13 @@
+package id.codelabs.codelabsapps_piket.ui.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import id.codelabs.codelabsapps_piket.R
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
